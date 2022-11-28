5130e6ddc49ff336f74513f29a4e6386365feb4b
David Ano-Trudeau <89054564+Gretron@users.noreply.github.com>
2022-11-28 01:38:07 -0500

    Update quizlog.sql

--- Source/htdocs/db/quizlog.sql
+++ Source/htdocs/db/quizlog.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 14, 2022 at 03:41 PM
+-- Generation Time: Nov 28, 2022 at 07:37 AM
 -- Server version: 10.4.24-MariaDB
 -- PHP Version: 8.1.6
 
@@ -39,26 +39,65 @@ CREATE TABLE `answer` (
 --
 
 INSERT INTO `answer` (`AnswerId`, `QuestionId`, `AnswerText`, `AnswerCorrect`) VALUES
-(34, 29, 'sadasd', 0),
-(35, 29, 'sdadsad', 1),
-(36, 29, 'asdasd', 0),
-(42, 32, 'dada', 0),
-(43, 32, 'adad', 1),
-(44, 32, 'adad', 0),
-(48, 34, 'sadsa', 1),
-(49, 34, 'dsadas', 0),
-(59, 38, 'nope', 1);
+(1148, 559, 'Zebra', 0),
+(1149, 559, 'Lion', 1),
+(1150, 559, 'Hyena', 0),
+(1151, 559, 'Vulture', 0),
+(1152, 560, 'hakuna,matata', 1);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `bookmark`
+-- Table structure for table `choice`
 --
 
-CREATE TABLE `bookmark` (
-  `QuizId` int(11) NOT NULL,
-  `UserId` int(11) NOT NULL
+CREATE TABLE `choice` (
+  `ChoiceId` int(11) NOT NULL,
+  `ResultId` int(11) NOT NULL,
+  `ChoiceType` enum('Multiple Choice','Short Answer') NOT NULL,
+  `QuestionText` text NOT NULL,
+  `QuestionImage` text NOT NULL,
+  `ChoiceText` text NOT NULL,
+  `CorrectText` text NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `choice`
+--
+
+INSERT INTO `choice` (`ChoiceId`, `ResultId`, `ChoiceType`, `QuestionText`, `QuestionImage`, `ChoiceText`, `CorrectText`) VALUES
+(78, 70, 'Multiple Choice', 'dsadsaaa', '', 'dsadsa', 'Answer 1'),
+(79, 71, 'Multiple Choice', 'TestTest', '637dae1caa57c.jpg', '', 'TestTestTest'),
+(80, 71, 'Multiple Choice', 'TestTest', '637dae1cb0f54.png', '', 'TestTest'),
+(81, 71, 'Multiple Choice', 'TestTest', '637dae1cb41cc.png', '', 'TestTestTestTest'),
+(82, 72, 'Multiple Choice', 'TestTest', '637dae680d08a.jpg', '', 'TestTestTest'),
+(83, 72, 'Multiple Choice', 'TestTest', '637dae6810447.png', '', 'TestTest'),
+(84, 72, 'Multiple Choice', 'TestTest', '637dae6812cf0.png', '', 'TestTestTestTest'),
+(85, 73, 'Multiple Choice', 'TestTest', '637dae8ed09c2.jpg', '', 'TestTestTest'),
+(86, 73, 'Multiple Choice', 'TestTest', '637dae8ed3efa.png', '', 'TestTest'),
+(87, 73, 'Multiple Choice', 'TestTest', '637dae8ed668f.png', '', 'TestTestTestTest'),
+(88, 74, 'Multiple Choice', 'TestTest', '637daeb5929fd.jpg', 'TestTestTest', 'TestTestTest'),
+(89, 74, 'Multiple Choice', 'TestTest', '637daeb6cc459.png', '', 'TestTest'),
+(90, 74, 'Multiple Choice', 'TestTest', '637daeb79607e.png', '', 'TestTestTestTest'),
+(91, 75, 'Short Answer', 'TestTestTest', '', 'Jam a man of fortune and j must seek its fortune', 'jam,a,man'),
+(92, 76, 'Short Answer', 'TestTestTest', '', 'cunt', 'jam,a,man'),
+(93, 77, 'Short Answer', 'TestTestTest', '', 'test', 'jam,a,man'),
+(94, 78, 'Short Answer', 'TestTestTest', '', 'testicule', 'jam,a,man'),
+(101, 83, 'Multiple Choice', 'TestTest', '637ff2de48687.jpg', 'TestTest', 'TestTestTest'),
+(102, 83, 'Multiple Choice', 'TestTest', '637ff2de49b58.png', 'TestTest', 'TestTest'),
+(103, 83, 'Multiple Choice', 'TestTest', '637ff2de4af9c.png', 'TestTest', 'TestTestTestTest'),
+(104, 84, 'Multiple Choice', 'dsadsaaa', '', 'Answer 1', 'Answer 1'),
+(105, 91, 'Multiple Choice', 'dsadasdasdsa', '6380346a633d1.png', '', 'sadsadas'),
+(115, 99, 'Multiple Choice', 'Which animal is in the picture?', '6381048b25094.jpg', 'Lion', 'Lion'),
+(116, 99, 'Short Answer', 'Which famous phrases is sung by this group in the Lion King movie?', '638104f37baee.jpg', 'hakuna matata', 'hakuna,matata'),
+(118, 100, 'Multiple Choice', 'Which animal is in the picture?', '6381048b25094.jpg', 'Lion', 'Lion'),
+(119, 100, 'Short Answer', 'Which famous phrases is sung by this group in the Lion King movie?', '638104f37baee.jpg', '', 'hakuna,matata'),
+(120, 101, 'Multiple Choice', 'Which animal is in the picture?', '6381048b25094.jpg', 'Lion', 'Lion'),
+(121, 101, 'Short Answer', 'Which famous phrases is sung by this group in the Lion King movie?', '638104f37baee.jpg', 'hakuna matata', 'hakuna,matata'),
+(123, 102, 'Multiple Choice', 'Which animal is in the picture?', '6381048b25094.jpg', 'Lion', 'Zebra'),
+(124, 102, 'Short Answer', 'Which famous phrases is sung by this group in the Lion King movie?', '638104f37baee.jpg', '', 'hakuna,matata'),
+(125, 103, 'Multiple Choice', 'Which animal is in the picture?', '6381048b25094.jpg', 'Lion', 'Lion'),
+(126, 103, 'Short Answer', 'Which famous phrases is sung by this group in the Lion King movie?', '638104f37baee.jpg', 'hakuna matata', 'hakuna,matata');
 
 -- --------------------------------------------------------
 
@@ -80,10 +119,8 @@ CREATE TABLE `question` (
 --
 
 INSERT INTO `question` (`QuestionId`, `QuizId`, `QuestionText`, `QuestionImage`, `QuestionHint`, `QuestionType`) VALUES
-(29, 40, 'sdadsa', 'Array', 'dsadsa', 'Multiple Choice'),
-(32, 42, 'dada', 'Array', 'dadada', 'Multiple Choice'),
-(34, 46, 'dsad', 'Array', 'sdadsa', 'Multiple Choice'),
-(38, 49, 'Do you like my sword?', 'Array', 'My Iron Sword..?', 'Short Answer');
+(559, 273, 'Which animal is in the picture?', '6381048b25094.jpg', 'He is the king of the jungle...', 'Multiple Choice'),
+(560, 273, 'Which famous phrases is sung by this group in the Lion King movie?', '638104f37baee.jpg', 'Be happy...', 'Short Answer');
 
 -- --------------------------------------------------------
 
@@ -97,7 +134,7 @@ CREATE TABLE `quiz` (
   `QuizName` varchar(128) NOT NULL,
   `QuizBanner` text NOT NULL,
   `QuizDescription` text NOT NULL,
-  `QuizPrivacy` tinyint(1) NOT NULL,
+  `QuizPrivacy` enum('0','1','2') NOT NULL,
   `QuizTime` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
@@ -106,12 +143,35 @@ CREATE TABLE `quiz` (
 --
 
 INSERT INTO `quiz` (`QuizId`, `UserId`, `QuizName`, `QuizBanner`, `QuizDescription`, `QuizPrivacy`, `QuizTime`) VALUES
-(40, 26, 'sdxsadsa', 'Array', 'dsadsa', 1, '00:00:00'),
-(42, 26, 'adada', 'Array', 'dadad', 0, '13:13:00'),
-(44, 26, 'dfsda', 'Array', 'sdadsa', 0, '00:00:00'),
-(45, 26, 'sdad', 'Array', 'sdadsad', 0, '00:00:00'),
-(46, 26, 'Borgir', 'Array', 'sdsad', 0, '00:00:00'),
-(49, 23, 'Borgir', 'Array', 'sdsad', 0, '01:00:00');
+(273, 23, 'Animal Kingdom Quiz', '63817165c1e55.jpg', 'This is a quiz about the animal kingdom. Your knowledge about the animal kingdom will be put to the test!', '0', '00:05:00');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `result`
+--
+
+CREATE TABLE `result` (
+  `ResultId` int(11) NOT NULL,
+  `UserId` int(11) NOT NULL,
+  `QuizId` int(11) NOT NULL,
+  `ResultName` varchar(128) NOT NULL,
+  `ResultMode` enum('Practice','Exam') NOT NULL,
+  `ResultImage` text NOT NULL,
+  `CurrentQuestion` int(11) NOT NULL,
+  `CompletedTime` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `result`
+--
+
+INSERT INTO `result` (`ResultId`, `UserId`, `QuizId`, `ResultName`, `ResultMode`, `ResultImage`, `CurrentQuestion`, `CompletedTime`) VALUES
+(99, 23, 0, 'Animal Kingdom Quiz', 'Practice', '6381048b2358a.jpg', 1, '2022-11-25 19:10:13'),
+(100, 23, 0, 'Animal Kingdom Quiz', 'Exam', '6381048b2358a.jpg', 1, '2022-11-25 19:11:19'),
+(101, 23, 0, 'Animal Kingdom Quiz', 'Practice', '6381048b2358a.jpg', 1, '2022-11-25 22:32:14'),
+(102, 23, 0, 'Animal Kingdom Quiz', 'Practice', '6381048b2358a.jpg', 1, '2022-11-25 22:32:58'),
+(103, 23, 0, 'Animal Kingdom Quiz', 'Practice', '63817165c1e55.jpg', 1, '2022-11-26 18:52:57');
 
 -- --------------------------------------------------------
 
@@ -153,11 +213,10 @@ ALTER TABLE `answer`
   ADD KEY `ANSWER_QUESTIONID_FK` (`QuestionId`);
 
 --
--- Indexes for table `bookmark`
+-- Indexes for table `choice`
 --
-ALTER TABLE `bookmark`
-  ADD KEY `BOOKMARK_QUIZ_FK` (`QuizId`),
-  ADD KEY `BOOKMARK_USER_FK` (`UserId`);
+ALTER TABLE `choice`
+  ADD PRIMARY KEY (`ChoiceId`);
 
 --
 -- Indexes for table `question`
@@ -174,6 +233,13 @@ ALTER TABLE `quiz`
   ADD KEY `QUIZ_USERID_FK` (`UserId`);
 
 --
+-- Indexes for table `result`
+--
+ALTER TABLE `result`
+  ADD PRIMARY KEY (`ResultId`),
+  ADD KEY `RESULT_USERID_FK` (`UserId`);
+
+--
 -- Indexes for table `user`
 --
 ALTER TABLE `user`
@@ -188,19 +254,31 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT for table `answer`
 --
 ALTER TABLE `answer`
-  MODIFY `AnswerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=60;
+  MODIFY `AnswerId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=1161;
+
+--
+-- AUTO_INCREMENT for table `choice`
+--
+ALTER TABLE `choice`
+  MODIFY `ChoiceId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=127;
 
 --
 -- AUTO_INCREMENT for table `question`
 --
 ALTER TABLE `question`
-  MODIFY `QuestionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=39;
+  MODIFY `QuestionId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=565;
 
 --
 -- AUTO_INCREMENT for table `quiz`
 --
 ALTER TABLE `quiz`
-  MODIFY `QuizId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `QuizId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=280;
+
+--
+-- AUTO_INCREMENT for table `result`
+--
+ALTER TABLE `result`
+  MODIFY `ResultId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=104;
 
 --
 -- AUTO_INCREMENT for table `user`
@@ -219,13 +297,6 @@ ALTER TABLE `answer`
   ADD CONSTRAINT `ANSWER_QUESTIONID_FK` FOREIGN KEY (`QuestionId`) REFERENCES `question` (`QuestionId`);
 
 --
--- Constraints for table `bookmark`
---
-ALTER TABLE `bookmark`
-  ADD CONSTRAINT `BOOKMARK_QUIZ_FK` FOREIGN KEY (`QuizId`) REFERENCES `quiz` (`QuizId`),
-  ADD CONSTRAINT `BOOKMARK_USER_FK` FOREIGN KEY (`UserId`) REFERENCES `user` (`UserId`);
-
---
 -- Constraints for table `question`
 --
 ALTER TABLE `question`
@@ -236,6 +307,12 @@ ALTER TABLE `question`
 --
 ALTER TABLE `quiz`
   ADD CONSTRAINT `QUIZ_USERID_FK` FOREIGN KEY (`UserId`) REFERENCES `user` (`UserId`);
+
+--
+-- Constraints for table `result`
+--
+ALTER TABLE `result`
+  ADD CONSTRAINT `RESULT_USERID_FK` FOREIGN KEY (`UserId`) REFERENCES `user` (`UserId`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
